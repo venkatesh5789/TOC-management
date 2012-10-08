7cd0bedd3e89aafbf92c0835e97b70a7a64eae69
Venkatesh Sriram <illuminati.assassin@gmail.com>
2012-10-08 12:40:42 -0400

    REST api added

--- my_website/app/controllers/visiting_companies_controller.rb
+++ my_website/app/controllers/visiting_companies_controller.rb
@@ -1,38 +1,24 @@
 class VisitingCompaniesController < ApplicationController
-  # GET /visiting_companies
-  # GET /visiting_companies.json
+
+  respond_to :html, :json, :xml
+
   def index
     @visiting_companies = VisitingCompany.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @visiting_companies }
-    end
+    respond_with @visiting_companies
   end
 
-  # GET /visiting_companies/1
-  # GET /visiting_companies/1.json
   def show
     @visiting_company = VisitingCompany.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @visiting_company }
-    end
+    respond_with @visiting_company
   end
 
-  # GET /visiting_companies/new
-  # GET /visiting_companies/new.json
   def new
     u = VisitingCompany.new
     u.student_id = params[:student_id]
     u.attending_company_id = params[:company_id]
     u.visited = false;
     u.save
-     respond_to do |format|
-      format.html{redirect_to Student.find(u.student_id)} # new.html.erb
-     # format.json { render json: u }
-    end
+    respond_with u
   end
 
   # GET /visiting_companies/1/edit
@@ -40,15 +26,12 @@ class VisitingCompaniesController < ApplicationController
     @visiting_company = VisitingCompany.find(params[:id])
   end
 
-  # POST /visiting_companies
-  # POST /visiting_companies.json
   def create
     @visiting_company = VisitingCompany.new(params[:visiting_company])
 
     respond_to do |format|
       if @visiting_company.save
-        format.html { redirect_to @visiting_company, notice: 'Visiting company was successfully created.' }
-        format.json { render json: @visiting_company, status: :created, location: @visiting_company }
+        respond_with @visiting_company
       else
         format.html { render action: "new" }
         format.json { render json: @visiting_company.errors, status: :unprocessable_entity }
@@ -56,8 +39,6 @@ class VisitingCompaniesController < ApplicationController
     end
   end
 
-  # PUT /visiting_companies/1
-  # PUT /visiting_companies/1.json
   def update
     @visiting_company = VisitingCompany.find(params[:id])
     @visiting_company.visited = !@visiting_company.visited
