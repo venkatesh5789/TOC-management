af213471a9e8611db0942b6e71f1cc07e12ea768
Venkatesh Sriram <illuminati.assassin@gmail.com>
2012-10-10 23:11:02 -0400

    final changes to rest

--- my_website/app/models/attending_company.rb
+++ my_website/app/models/attending_company.rb
@@ -1,3 +1,4 @@
 class AttendingCompany < ActiveRecord::Base
+  attr_accessible :name, :day1, :day2, :specialization
   has_many :students
 end
--- my_website/app/models/visiting_company.rb
+++ my_website/app/models/visiting_company.rb
@@ -1,3 +1,4 @@
 class VisitingCompany < ActiveRecord::Base
+  attr_accessible  :student_id, :attending_company_id, :visited
   belongs_to :student
   end
